4fbec31c719dcb400c48d0c7cc0d726d4aca2d5c
Shripad K <assortmentofsorts@gmail.com>
2012-11-07 19:28:28 +0530

    refactored js port

--- js/index.js
+++ js/index.js
@@ -9,9 +9,14 @@ regexes = yaml.eval(regexes);
 var ua_parsers = regexes.user_agent_parsers.map(function(obj) {
   var regexp = new RegExp(obj.regex),
       famRep = obj.family_replacement,
-      majorVersionRep = obj.v1_replacement;
+      majorVersionRep = obj.v1_replacement,
+      minorVersionRep = obj.v2_replacement;
 
   function parser(ua) {
+    if(!ua || !ua.length) {
+      throw new Error('UserAgent not specified');
+    }
+
     var m = ua.match(regexp);
     
     if (!m) { return null; }
@@ -19,9 +24,10 @@ var ua_parsers = regexes.user_agent_parsers.map(function(obj) {
     var family = famRep ? famRep.replace('$1', m[1]) : m[1];
     
     var obj = new UserAgent(family);
-    obj.major = parseInt(majorVersionRep ? majorVersionRep : m[2]);
-    obj.minor = m[3] ? parseInt(m[3]) : null;
-    obj.patch = m[4] ? parseInt(m[4]) : null;
+
+    obj.major = (majorVersionRep ? majorVersionRep : m[2]) || '';
+    obj.minor = (minorVersionRep ? minorVersionRep : m[3]) || '';
+    obj.patch = m[4] || '';
     
     return obj;
   }
@@ -31,14 +37,21 @@ var ua_parsers = regexes.user_agent_parsers.map(function(obj) {
 
 var os_parsers = regexes.os_parsers.map(function(obj) {
   var regexp = new RegExp(obj.regex),
-      osRep  = obj.os_replacement;
+      osRep  = obj.os_replacement,
+      minorVersionRep = obj.os_v1_replacement,
+      majorVersionRep = obj.os_v2_replacement;
 
   function parser(ua) {
     var m = ua.match(regexp);
 
     if(!m) { return null; }
 
-    var os = (osRep ? osRep : m[1]) + (m.length > 2 ? " " + m[2] : "") + (m.length > 3 ? "." + m[3] : "");
+    var os = {
+      family: (osRep ? osRep.replace('$1', m[1]) : m[1]) || '',
+      major : (majorVersionRep ? majorVersionRep : m[2]) || '',
+      minor : (minorVersionRep ? minorVersionRep : m[3]) || '',
+      patch : m[4] || ''
+    };
 
     return os;
   }
@@ -48,49 +61,73 @@ var os_parsers = regexes.os_parsers.map(function(obj) {
 
 exports.parse = parse;
 function parse(ua) {
-  var os, i;
-  for (i=0; i < ua_parsers.length; i++) {
-    var result = ua_parsers[i](ua);
-    if (result) { break; }
-  }
+  var result, os, i;
 
-  for (i=0; i < os_parsers.length; i++) {
-    os = os_parsers[i](ua);
-    if (os) { break; }
-  }
+  ua_parsers.some(function(u) {
+    return result = u(ua);
+  });
+
+  os_parsers.some(function(o) {
+    return os = o(ua);
+  });
 
   if(!result) { result = new UserAgent(); }
 
-  result.os = os;
+  if(os) {
+    result.os.family = os.family;
+    result.os.major  = os.major;
+    result.os.minor  = os.minor;
+    result.os.patch  = os.patch;
+  }
   return result;
 }
 
-function UserAgent(family) {
-  this.family = family || 'Other';
-}
-
-UserAgent.prototype.toVersionString = function() {
+function toVersionString() {
   var output = '';
-  if (this.major != null) {
+  if (this.major.length) {
     output += this.major;
-    if (this.minor != null) {
+    if (this.minor.length) {
       output += '.' + this.minor;
-      if (this.patch != null) {
+      if (this.patch.length) {
         output += '.' + this.patch;
       }
     }
   }
-  return output;
-};
+  return output;  
+}
 
-UserAgent.prototype.toString = function() {
+function toString() {
   var suffix = this.toVersionString();
   if (suffix) { suffix = ' ' + suffix; }
   return this.family + suffix;
-};
+}
+
+function OS() {
+  this.family = 'Other';
+  this.major  = '';
+  this.minor  = '';
+  this.patch  = '';
+}
+
+OS.prototype.toVersionString = toVersionString;
+
+OS.prototype.toString = toString;
+
+function UserAgent(family) {
+  this.os = new OS();
+
+  this.family = family || 'Other';
+  this.major = '';
+  this.minor = '';
+  this.patch = '';
+}
+
+UserAgent.prototype.toVersionString = toVersionString;
+
+UserAgent.prototype.toString = toString;
 
 UserAgent.prototype.toFullString = function() {
-  return this.toString() + (this.os ? "/" + this.os : "");
+  return this.toString() + (this.os.toString() ? "/" + this.os.toString() : "");
 };
 
 if (require.main === module) {
