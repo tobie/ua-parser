b3656168f20aa90447e224e3fe6d8d0caa0be940
Tobie Langel <tobie.langel@gmail.com>
2012-11-12 18:03:19 +0100

    Quote consistency.

--- js/test/old-api.js
+++ js/test/old-api.js
@@ -5,16 +5,16 @@ var assert = require('assert'),
 
 var USER_AGENT_STRING = 'Mozilla/5.0 (Windows NT 6.1; rv:2.0b6pre) Gecko/20100903 Firefox/4.0b6pre Firefox/4.0b6pre';
 
-suite('parse function', function() {  
-  test("output with valid UA string", function() {
+suite('parse function', function() {
+  test('output with valid UA string', function() {
     var output = parse(USER_AGENT_STRING);
     assert.strictEqual(output.family, 'Firefox Beta');
     assert.strictEqual(output.major, 4);
     assert.strictEqual(output.minor, 0);
     assert.ok(isNaN(output.patch));
   });
-  
-  test("output with invalid UA string", function() {
+
+  test('output with invalid UA string', function() {
     var output = parse('');
     assert.strictEqual(output.family, 'Other');
     assert.strictEqual(output.major, undefined);
