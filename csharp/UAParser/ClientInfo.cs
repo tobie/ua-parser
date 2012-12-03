747703f8c95d2df60250ea373b3ec4e90b477fa8
Søren Enemærke <soren.enemaerke@gmail.com>
2012-12-03 21:54:22 +0100

    Adding comments

--- csharp/UAParser/ClientInfo.cs
+++ csharp/UAParser/ClientInfo.cs
@@ -5,22 +5,44 @@ using System.Text;
 
 namespace UAParser
 {
-    public class ClientInfo
+  /// <summary>
+  /// Represents the various parts of information extracted from a user agent string
+  /// </summary>
+  public class ClientInfo
+  {
+    /// <summary>
+    /// The operating system information
+    /// </summary>
+    public OS OS { get; private set; }
+    /// <summary>
+    /// The device information
+    /// </summary>
+    public Device Device { get; private set; }
+    /// <summary>
+    /// The user agent information
+    /// </summary>
+    public UserAgent UserAgent { get; private set; }
+
+    /// <summary>
+    /// Constructing the client information
+    /// </summary>
+    /// <param name="os">the operating system information</param>
+    /// <param name="device">the device information</param>
+    /// <param name="userAgent">the user agent information</param>
+    public ClientInfo(OS os, Device device, UserAgent userAgent)
     {
-        public OS OS { get; private set; }
-        public Device Device { get; private set; }
-        public UserAgent UserAgent { get; private set; }
-
-        public ClientInfo(OS os, Device device, UserAgent userAgent)
-        {
-            OS = os;
-            Device = device;
-            UserAgent = userAgent;
-        }
-
-        public override string ToString()
-        {
-            return string.Format("{0} {1} {2}", OS, Device, UserAgent);
-        }
+      OS = os;
+      Device = device;
+      UserAgent = userAgent;
     }
+
+    /// <summary>
+    /// Represents a collective string representation of the client information
+    /// </summary>
+    /// <returns>A string representation of the client information</returns>
+    public override string ToString()
+    {
+      return string.Format("{0} {1} {2}", OS, Device, UserAgent);
+    }
+  }
 }
--- csharp/UAParser/UserAgent.cs
+++ csharp/UAParser/UserAgent.cs
@@ -5,30 +5,52 @@ using System.Text;
 
 namespace UAParser
 {
-    public class UserAgent
+  /// <summary>
+  /// The UserAgent extracted from the user agent string
+  /// </summary>
+  public class UserAgent
+  {
+    internal UserAgent() { }
+    internal UserAgent(string family, string major, string minor, string patch)
     {
-        public UserAgent() { }
-        public UserAgent(string family, string major, string minor, string patch)
-        {
-            Family = family;
-            Major = major;
-            Minor = minor;
-            Patch = patch;
-        }
-        public string Family { get; set; }
-        public string Major { get; set; }
-        public string Minor { get; set; }
-        public string Patch { get; set; }
-
-        public override string ToString()
-        {
-            return string.Format("UserAgent: {0} {1}.{2} {3}", Family, Major, Minor, Patch);
-        }
-
-        public string ToVersionString()
-        {
-          IEnumerable<string> versions = new string[] { Major, Minor, Patch }.Where(x => !string.IsNullOrEmpty(x));
-          return string.Join(".", versions.ToArray());
-        }
+      Family = family;
+      Major = major;
+      Minor = minor;
+      Patch = patch;
     }
+    /// <summary>
+    /// The family of the user agent
+    /// </summary>
+    public string Family { get; set; }
+    /// <summary>
+    /// The major version of the user agent
+    /// </summary>
+    public string Major { get; set; }
+    /// <summary>
+    /// The minor version of the user agent
+    /// </summary>
+    public string Minor { get; set; }
+    /// <summary>
+    /// The patch version of the user agent
+    /// </summary>
+    public string Patch { get; set; }
+
+    /// <summary>
+    /// A string representation of the user agent
+    /// </summary>
+    /// <returns>string representation of the user agent</returns>
+    public override string ToString()
+    {
+      return string.Format("UserAgent: {0} {1}.{2} {3}", Family, Major, Minor, Patch);
+    }
+    /// <summary>
+    /// The version of the user agent as a single string
+    /// </summary>
+    /// <returns>the version as a string</returns>
+    public string ToVersionString()
+    {
+      IEnumerable<string> versions = new string[] { Major, Minor, Patch }.Where(x => !string.IsNullOrEmpty(x));
+      return string.Join(".", versions.ToArray());
+    }
+  }
 }
