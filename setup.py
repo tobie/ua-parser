f58d423fd7557bee03ef74197c01e44b901b1654
Selwin Ong <selwin.ong@gmail.com>
2012-12-28 15:06:55 +0700

    Run the same compilation command when installed via git clone.

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     install_requires=[
         'pyyaml',
     ],
-    cmdclass={'install': install},
+    cmdclass={'install': install, 'develop': install},
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
