8b2770d2a7dcad2b0f20c727b2072a0ef3443a5f
Selwin Ong <selwin.ong@gmail.com>
2012-12-28 10:19:34 +0700

    Prepare Python port for PyPI release.

--- setup.py
+++ setup.py
@@ -2,16 +2,30 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ua_parser',
-    version='1.0',
-    description='',
+    name='ua-parser',
+    version='0.3.0',
+    description="Python port of Browserscope's user agent parser",
     author='PBS',
     author_email='no-reply@pbs.org',
-    packages = find_packages('py'),
+    packages=find_packages('py'),
     package_dir={'':'py'},
+    license='LICENSE.txt',
+    zip_safe=False,
+    url='https://github.com/tobie/ua-parser',
     include_package_data=True,
+    package_data = { '': ['README.markdown'] },
     data_files=[('',['regexes.yaml'])],
     install_requires = [
         'pyyaml',
-    ]
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
