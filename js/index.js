5d30bcf92d10f7f9e2451777a02249a69e10682d
Nikolay S. Frantsev <code@frantsev.ru>
2013-01-11 13:07:30 +0200

    Refactored index for JS-parser

--- js/index.js
+++ js/index.js
@@ -1,32 +1,34 @@
-var path = require('path'), 
-    fs = require('fs'),
-    yaml = require('yamlparser'),
-    Results = require('./lib/results').BackwardsCompatResults;
+var path = require('path'),
+  fs = require('fs'),
+  yamlparser = require('yamlparser'),
 
-var file = path.join(__dirname, '..', 'regexes.yaml'),
-    regexes = fs.readFileSync(file, 'utf8');
+  results = require('./lib/results'),
+  ua = require('./lib/ua'),
+  os = require('./lib/os'),
+  device = require('./lib/device'),
 
-regexes = yaml.eval(regexes);
+  regexes = yamlparser.eval(
+    fs.readFileSync(
+      path.join(__dirname, '..', 'regexes.yaml'),
+    'utf8')
+  ),
 
-var parseUA = require('./lib/ua').makeParser(regexes.user_agent_parsers);
-exports.parseUA = parseUA;
+  parseUA =
+    exports.parseUA = ua.makeParser(regexes.user_agent_parsers),
+  parseOS =
+    exports.parseOS = os.makeParser(regexes.os_parsers),
+  parseDevice =
+    exports.parseDevice = device.makeParser(regexes.device_parsers);
 
-var parseOS = require('./lib/os').makeParser(regexes.os_parsers);
-exports.parseOS = parseOS;
-
-var parseDevice = require('./lib/device').makeParser(regexes.device_parsers);
-exports.parseDevice = parseDevice;
-
-exports.parse = parse;
-function parse(str) {
-  var ua = parseUA(str),
-      os = parseOS(str),
-      device = parseDevice(str);
-  return new Results(str, ua, os, device);
-}
+exports.parse = function(str) {
+  return new results.BackwardsCompatResults(str,
+    parseUA(str),
+      parseOS(str),
+        parseDevice(str));
+};
 
 if (require.main === module) {
-  var output, input = process.argv[2];
-  if (!input) { process.exit(1); }
-  process.stdout.write(parseUA(input).toString());
+  process.stdout.write(
+    parseUA(process.argv[2] || process.exit(1)).toString()
+  );
 }
