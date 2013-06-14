0ea6b713aa7a4ffbc19568b5f85dd1a052fcbcee
George V. Reilly <george@reilly.org>
2013-06-14 14:21:14 -0700

    Fix 'python setup.py install' on Linux; clean up.
    * 'python setup.py install' works
    * 'python setup.py develop' works
    * 'pip install ua-parser' does NOT work (yet)

--- setup.py
+++ setup.py
@@ -2,54 +2,65 @@
 import os
 import shutil
 
-from pkg_resources import resource_filename
-from setuptools import setup, find_packages
-from setuptools.command.install import install as _install
-from setuptools.command.build_py import build_py as _build_py
+from setuptools import setup
+from setuptools.command.develop  import develop  as _develop
+from setuptools.command.install  import install  as _install
+from setuptools.command.sdist    import sdist    as _sdist   
 
+def install_regexes():
+    # copy regexes.yaml down into the package directory
+    print 'Copying regexes.yaml to package directory...'
+    cwd = os.path.abspath(os.path.dirname(__file__))
+    yaml_src = os.path.join(cwd, 'regexes.yaml')
+    if not os.path.exists(yaml_src):
+        raise RuntimeError(
+                  'Unable to find regexes.yaml, should be at %s' % yaml_src)
+    yaml_dest = os.path.join(cwd, 'py', 'ua_parser', 'regexes.yaml')
+    shutil.copy(yaml_src, yaml_dest)
+    convert_regexes_yaml_to_json(yaml_dest)
+
+def convert_regexes_yaml_to_json(yaml_dest):
+    print 'Converting regexes.yaml to json...'
+    import json
+    import yaml
+    regexes = yaml.load(open(yaml_dest))
+    json_dest = yaml_dest.replace('.yaml', '.json')
+    json.dump(regexes, open(json_dest, 'w'))
+
+class develop(_develop):
+    def run(self):
+        install_regexes()
+        _develop.run(self)
 
 class install(_install):
     def run(self):
-        # copy regexes.yaml down into the package directory
-        print 'Copying regexes.yaml to package directory...'
-        cwd = os.path.abspath(os.path.dirname(__file__))
-        yaml_src = os.path.join(cwd, 'regexes.yaml')
-        if not os.path.exists(yaml_src):
-            raise RuntimeError(
-                      'Unable to find regexes.yaml, should be at %s' % yaml_src)
-        yaml_dest = os.path.join(cwd, 'py', 'ua_parser', 'regexes.yaml')
-        shutil.copy(yaml_src, yaml_dest)
-
-        # convert yaml to json
-        print 'Converting regexes.yaml to json...'
-        import json
-        import yaml
-        regexes = yaml.load(open(yaml_dest))
-        json_dest = yaml_dest.replace('.yaml', '.json')
-        json.dump(regexes, open(json_dest, 'w'))
+        install_regexes()
         _install.run(self)
 
-class build_py(_build_py):
+class sdist(_sdist):
     def run(self):
-        self.data_files.append(
-            ('ua_parser', '', 'build/lib/ua_parser', ['regexes.yaml', 'regexes.json']) )
-        _build_py.run(self)
+        install_regexes()
+        _sdist.run(self)
 
 setup(
     name='ua-parser',
-    version='0.3.3',
+    version='0.3.3.1',
     description="Python port of Browserscope's user agent parser",
     author='PBS',
     author_email='no-reply@pbs.org',
-    packages=find_packages('py'),
+    packages=['ua_parser'],
     package_dir={'': 'py'},
     license='LICENSE.txt',
     zip_safe=False,
     url='https://github.com/tobie/ua-parser',
     include_package_data=True,
-    package_data={'': ['regexes.yaml', 'regexes.json']},
+    package_data={'ua_parser': ['regexes.yaml', 'regexes.json']},
     install_requires=['pyyaml'],
-    cmdclass={'install': install, 'develop': install, 'build_py': build_py},
+    cmdclass={
+        'develop': develop,
+        'install': install,
+        'sdist':   sdist,
+    },
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
