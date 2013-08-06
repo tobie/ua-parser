b2bc3c14ea7d140abf70634eb9d780b939edb3a5
commenthol <commenthol@prrtr.de>
2013-08-06 08:18:18 +0200

    brand model parsing

--- js/lib/device.js
+++ js/lib/device.js
@@ -1,24 +1,35 @@
 exports.Device = Device
-function Device(family) {
+function Device(family, brand, model) {
   this.family = family || 'Other';
+  this.brand = brand || null;
+  this.model = model || null;
 }
 
 Device.prototype.toString = function() {
   return this.family;
 };
 
+function multiReplace(str, m) {
+  return str.replace(/\$(\d)/g, function(tmp, i) {
+    return m[i] || '';
+  });
+}
 
 exports.makeParser = function(regexes) {
   var parsers = regexes.map(function (obj) {
     var regexp = new RegExp(obj.regex),
-        deviceRep = obj.device_replacement;
+        deviceRep = obj.device_replacement,
+        brandRep = obj.brand_replacement,
+        modelRep = obj.model_replacement;
 
     function parser(str) {
       var m = str.match(regexp);
       if (!m) { return null; }
 
-      var family = deviceRep ? deviceRep.replace('$1', m[1]) : m[1];
-      return new Device(family);
+      var family = deviceRep ? multiReplace(deviceRep, m) : m[1];
+      var brand  = brandRep  ? multiReplace(brandRep, m)  : null;
+      var model  = modelRep  ? multiReplace(modelRep, m)  : m[1];
+      return new Device(family, brand, model);
     }
 
     return parser;
