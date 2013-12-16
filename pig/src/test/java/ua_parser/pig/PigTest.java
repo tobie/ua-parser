5bec8678d5794942bf0ffe011d32e7473a9cd201
Niels Basjes <niels@basjes.nl>
2013-12-16 14:37:51 +0100

    Added brand and model parsing to Pig implementation

--- pig/src/test/java/ua_parser/pig/PigTest.java
+++ pig/src/test/java/ua_parser/pig/PigTest.java
@@ -10,13 +10,15 @@ import org.junit.Test;
 
 public class PigTest extends TestCase {
     @Test
-    public void testPigFuncs() throws IOException {
+    public void testPigFuncsSeparateFields() throws IOException {
         Tuple tuple = TupleFactory.getInstance().newTuple(1);
         tuple.set(0, "Mozilla/5.0 (iPhone; CPU iPhone OS 5_1_3 like Mac OS X) "
                 + "AppleWebKit/534.46 (KHTML, like Gecko) "
                 + "Version/5.1 Mobile/9B179 Safari/7534.48.3");
 
         assertEquals("iPhone",          (new ua_parser.pig.device.Family()).exec(tuple));
+        assertEquals("Apple",           (new ua_parser.pig.device.Brand()).exec(tuple));
+        assertEquals("iPhone",          (new ua_parser.pig.device.Model()).exec(tuple));
 
         assertEquals("iOS",             (new ua_parser.pig.os.Family()).exec(tuple));
         assertEquals("5",               (new ua_parser.pig.os.Major()).exec(tuple));
@@ -31,6 +33,8 @@ public class PigTest extends TestCase {
 
         // Run the same set twice
         assertEquals("iPhone",          (new ua_parser.pig.device.Family()).exec(tuple));
+        assertEquals("Apple",           (new ua_parser.pig.device.Brand()).exec(tuple));
+        assertEquals("iPhone",          (new ua_parser.pig.device.Model()).exec(tuple));
 
         assertEquals("iOS",             (new ua_parser.pig.os.Family()).exec(tuple));
         assertEquals("5",               (new ua_parser.pig.os.Major()).exec(tuple));
@@ -44,6 +48,24 @@ public class PigTest extends TestCase {
         assertEquals(null,              (new ua_parser.pig.useragent.Patch()).exec(tuple));
     }
 
+    @Test
+    public void testPigFuncsPerTypeCombined() throws IOException {
+        Tuple tuple = TupleFactory.getInstance().newTuple(1);
+        tuple.set(0, "Mozilla/5.0 (iPhone; CPU iPhone OS 5_1_3 like Mac OS X) "
+                + "AppleWebKit/534.46 (KHTML, like Gecko) "
+                + "Version/5.1 Mobile/9B179 Safari/7534.48.3");
+
+        assertEquals("(iPhone,Apple,iPhone)",  (new ua_parser.pig.Device()).exec(tuple).toString());
+        assertEquals("(iOS,5,1,3,)",           (new ua_parser.pig.Os()).exec(tuple).toString());
+        assertEquals("(Mobile Safari,5,1,)",   (new ua_parser.pig.UserAgent()).exec(tuple).toString());
+
+        // Run the same set twice
+        assertEquals("(iPhone,Apple,iPhone)",  (new ua_parser.pig.Device()).exec(tuple).toString());
+        assertEquals("(iOS,5,1,3,)",           (new ua_parser.pig.Os()).exec(tuple).toString());
+        assertEquals("(Mobile Safari,5,1,)",   (new ua_parser.pig.UserAgent()).exec(tuple).toString());
+    }
+
+    
     @Test
     public void testNullInput() throws IOException {
 
