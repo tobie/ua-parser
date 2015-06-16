7995a0f6a7358fd1f5db3236dca26dabaa61aa4b
Andrea Di Persio <andrea.dipersio@gmail.com>
2015-06-16 17:39:02 +0200

    Fix typo in go example.go

--- go/example/example.go
+++ go/example/example.go
@@ -1,12 +1,21 @@
 package main
 
 import (
-	"../uaparser" // You could change this to a github repo as well
 	"fmt"
+	"io/ioutil"
+	"os" // You could change this to a github repo as well
+
+	"../uaparser"
 )
 
 func main() {
-	testStr := "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_3; en-us; Silk/1.1.0-80) AppleWebKit/533.16 (KHTML, like Gecko) Version/5.0 Safari/533.16 Silk-Accelerated=true"
+	testBytes, err := ioutil.ReadAll(os.Stdin)
+
+	if err != nil {
+		panic(err)
+	}
+
+	testStr := string(testBytes)
 	regexFile := "../../regexes.yaml"
 	parser := uaparser.New(regexFile)
 	client := parser.Parse(testStr)
